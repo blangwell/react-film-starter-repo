1d179039a84eb25d117f5016576ef7ec5e6d1eb6
blangwell <b.langwell@outlook.com>
2021-02-05 23:41:36 -0800

    Complete part four

--- src/App.js
+++ src/App.js
@@ -3,11 +3,22 @@ import './App.css';
 import FilmListing from './FilmListing';
 import FilmDetails from './FilmDetails';
 import TMDB from './TMDB';
+import axios from 'axios';
+
+const popularFilmsUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB.api_key}&language=en-US&page=1`;
 
 class App extends Component {
   state = {
     films: TMDB.films,
     current: {}
+  }
+  componentDidMount = () => {
+    console.log('component mounted')
+    axios.get(popularFilmsUrl)
+    .then(response => {
+      this.setState({films: response.data.results})
+      console.log(response.data.results)
+    })
   }
   handleDetailsClick = (film) => {
     console.log(`🦮 fetching details for ${film.title}`);
